0c2428e4fb152a4ba4814fce0dbb9e29e8a56892
Gale Bonker <gbonker@andrew.cmu.edu>
2016-02-29 13:02:28 -0500

    got login to work, somewhat fixed nav bar, and updated foundation to the latest version

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ActiveRecord::Base
   #Validations
   validates_presence_of :name, :email
   validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"
+  validates_presence_of :password, on: :create 
+  validates_presence_of :password_confirmation, on: :create 
+  validates_confirmation_of :password, message: "does not match"
+  validates_length_of :password, minimum: 4, message: "must be at least 4 characters long", allow_blank: true
 
+# login by email address
+  def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+  end
 
 end
