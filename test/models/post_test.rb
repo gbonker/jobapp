60c1da0a134beed8656352b16868eb31fa256fdd
Gale Bonker <gbonker@andrew.cmu.edu>
2016-03-02 03:50:11 -0500

    added some more unit tests and they all pass

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -12,4 +12,12 @@ class PostTest < ActiveSupport::TestCase
   should validate_presence_of(:title)
   should validate_presence_of(:description)
 
+  should allow_value("UX Designer").for(:title)
+  should allow_value("Software Developer").for(:title)
+  should_not allow_value(nil).for(:title)
+
+  should allow_value("lorem ipsum").for(:description)
+  should allow_value("sdjfljsdlkfjskljdflksjdlkfjslkdjfksjdlkfjslkdjfsdjf").for(:description)
+  should_not allow_value(nil).for(:description)
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,8 +11,19 @@ class UserTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
   should validate_presence_of(:email)
 
+  should allow_value("Gale").for(:name)
+  should allow_value("gbonks wunk").for(:name)
+  should allow_value("Gale Virginia Bonker").for(:name)
+  should_not allow_value(nil).for(:name)
+
   should allow_value("gbonker@gmail.com").for(:email)
   should allow_value("gbonker@andrew.cmu.edu").for(:email)
+  should allow_value("gbonker@example.com").for(:email)
   should_not allow_value("bad").for(:email)
+  should_not allow_value("bad@").for(:email)
+  should_not allow_value("bad@example").for(:email)
+  should_not allow_value("@bad@example").for(:email)
+  should_not allow_value("@bad@example.com").for(:email)
+  should_not allow_value(nil).for(:email)
 
 end
